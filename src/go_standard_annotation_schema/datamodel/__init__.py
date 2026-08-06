989283e578187173e5fa7b8eadc277d314c82ecf
Patrick Kalita <pkalita@lbl.gov>
2026-08-05 17:11:15 -0700

    Add Entity class

--- src/go_standard_annotation_schema/datamodel/__init__.py
+++ src/go_standard_annotation_schema/datamodel/__init__.py
@@ -2,9 +2,25 @@
 
 from pathlib import Path
 
-from .go_standard_annotation_schema import *  # noqa: F403
+from .go_standard_annotation_schema import (
+    Annotation,
+    AnnotationExtension,
+    AnnotationProperty,
+    Entity,
+    EntityProperty,
+    Property,
+)
 
 THIS_PATH = Path(__file__).parent
 
 SCHEMA_DIRECTORY = THIS_PATH.parent / "schema"
 MAIN_SCHEMA_PATH = SCHEMA_DIRECTORY / "go_standard_annotation_schema.yaml"
+
+__all__ = [
+    "Annotation",
+    "AnnotationExtension",
+    "AnnotationProperty",
+    "Entity",
+    "EntityProperty",
+    "Property",
+]
